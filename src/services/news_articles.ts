4c40602469a408c81e1a8495b2069e9ea2cf8644
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-03-26 19:49:16 +0000

    Update news_articles.ts

--- src/services/news_articles.ts
+++ src/services/news_articles.ts
@@ -1,5 +1,5 @@
-import { supabase } from './supabase';
-import { Article } from '../types';
+import { supabase } from './supabase.js';
+import { Article } from '../types.js';
 
 export async function fetchNewsArticles(categories?: string[]): Promise<Article[]> {
   let query = (supabase
