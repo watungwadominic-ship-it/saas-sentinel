b9283dba0952f0e3620742d0c3a55eba1c098838
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-03-26 13:37:57 +0000

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL || 'https://dpwkojtfeoxlpyevutfc.supabase.co';
 const supabaseKey = process.env.VITE_SUPABASE_KEY || process.env.SUPABASE_KEY || 'sb_publishable_WumEuqpPeooXrt1nkO9l_w_zWa37BgE';
 
+// createClient doesn't crash on startup if strings are provided, even if invalid.
 export const supabase = createClient(supabaseUrl, supabaseKey);
