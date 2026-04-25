ee685acbcb17b8c85746dec90cad9bfe59b46708
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-04-25 16:46:08 +0100

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://dpwkojtfeoxlpyevutfc.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || 'sb_publishable_WumEuqpPeooXrt1nkO9l_w_zWa37BgE';
+const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || 'https://dpwkojtfeoxlpyevutfc.supabase.co';
+const supabaseKey = (import.meta as any).env.VITE_SUPABASE_KEY || 'sb_publishable_WumEuqpPeooXrt1nkO9l_w_zWa37BgE';
 
 // createClient doesn't crash on startup if strings are provided, even if invalid.
 export const supabase = createClient(supabaseUrl, supabaseKey);
