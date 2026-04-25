6146ab740ec9f9fbecf02cafde1928dc32fc947f
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-04-25 17:01:50 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App';
+import App, { ErrorBoundary } from './App';
 import './index.css';
 
 if (typeof window !== 'undefined' && !window.process) {
   // @ts-ignore
   window.process = { env: {} };
 }
-
-import {ErrorBoundary} from './components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
