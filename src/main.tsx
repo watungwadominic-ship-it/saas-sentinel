ca8ee43aa7474bb080bd9d94284474c99f280410
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-04-25 18:35:11 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import {createRoot} from 'react-dom/client';
 import App, { ErrorBoundary } from './App';
 import './index.css';
 
-if (typeof window !== 'undefined' && !window.process) {
+if (typeof window !== 'undefined') {
   // @ts-ignore
-  window.process = { env: {} };
+  window.process = window.process || {};
+  // @ts-ignore
+  window.process.env = window.process.env || {};
+  // @ts-ignore
+  window.global = window.global || window;
 }
 
 createRoot(document.getElementById('root')!).render(
