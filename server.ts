5b3a1240aa120f10184dd144f66ee8100c6d21db
watungwadominic-ship-it <watungwadominic@gmail.com>
2026-04-29 13:27:54 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -56,6 +56,9 @@ app.get(["/sitemap.xml", "/api/sitemap.xml"], async (req, res) => {
     
     let sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
     sitemap += `\n  <url><loc>${base}/</loc><changefreq>hourly</changefreq><priority>1.0</priority></url>`;
+    sitemap += `\n  <url><loc>${base}/archive</loc><changefreq>daily</changefreq><priority>0.8</priority></url>`;
+    sitemap += `\n  <url><loc>${base}/about</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>`;
+    sitemap += `\n  <url><loc>${base}/privacy</loc><changefreq>monthly</changefreq><priority>0.3</priority></url>`;
 
     if (articles) {
       articles.forEach((a: any) => {
